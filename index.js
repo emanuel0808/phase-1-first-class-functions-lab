28ff310935cc6c12f4bf6305515322bcf09426bd
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-04-14 13:48:58 -0700

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return [`Antonia`, `Nuru`];
+}
+
+function returnLastTwoDrivers(drivers){
+    return ['Amari', 'Mo'];
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers ];
+
+function createFareMultiplier(){
+    return function(number){
+        return number * number;
+    }
+}
+
+function fareDoubler(number){
+    return number * 2;
+
+}
+
+function fareTripler(number){
+    return number * 3;
+
+}
+
+function selectDifferentDrivers(arrayOfDrivers,drivers){
+    return drivers(arrayOfDrivers);
+}
+
